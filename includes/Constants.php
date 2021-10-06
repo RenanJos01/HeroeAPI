70a03278d35081e7e4c29d4b3652b9a651e0bf08
Renan <renan.oliveira267@etecia.com.br>
2021-10-06 17:41:16 -0300

    URL no Postman

--- includes/Constants.php
+++ includes/Constants.php
@@ -2,6 +2,6 @@
 
 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'etec');
-	define('DB_PASS', '1234567');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
 	define('DB_NAME', 'android');
